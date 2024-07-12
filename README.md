5882903e21e6286658583cc91579860e7301e22b
postvlone <truhanovichna1@gmail.com>
2024-07-12 18:56:07 +0300

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# homework_1
+# My data science projects
+From the [Skillfactory Data Science course](https://skillfactory.ru/data-scientist)
+
+## Проекты
+
+* [Проект 0. Игра: Угадай число](https://github.com/postvlone/data/tree/main/project_0)
+* [Проект 1. _______](______)
+* [Проект 2. _______](______)
--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -34,14 +34,14 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print(f"Алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
 
